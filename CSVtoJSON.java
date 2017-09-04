d8393670c6401764199f09bcce8ba72c2d2d7848
Vidur Chanana <vidurchanana@hotmail.com>
2017-09-04 16:50:52 -0700

    Add files via upload

---
+++ CSVtoJSON.java
@@ -0,0 +1,59 @@
+package com.personalcapital.CodingExercise;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class CSVtoJSON {
+	
+	public static void main(String[] args) throws Exception{
+		
+		
+		String FILENAME1 = "C:\\Users\\vidur\\Desktop\\csvdata2500.json";
+		File file1 = new File(FILENAME1);
+		
+		FileWriter writer = new FileWriter(file1, true);
+		
+		Class.forName("org.relique.jdbc.csv.CsvDriver");
+		
+	    // Create a connection. The first command line parameter is
+	    // the directory containing the .csv files.
+	    // A single connection is thread-safe for use by several threads.
+	    Connection conn = DriverManager.getConnection("jdbc:relique:csv:" + "C:\\Users\\vidur\\Desktop");	
+	    // Create a Statement object to execute the query with.
+	    // A Statement is not thread-safe.
+	    Statement stmt = conn.createStatement();
+	    String query= "SELECT * FROM csvdata LIMIT 2500 OFFSET 1";
+	    // Select the ID and NAME columns from sample.csv
+	    ResultSet results = stmt.executeQuery(query);
+	    
+	    //JSONArray json = new JSONArray();
+	    ResultSetMetaData metadata = results.getMetaData();
+	    int numColumns = metadata.getColumnCount();
+	    int count=1;
+	    while(results.next())             //iterate rows
+	    {
+	    	
+	        JSONObject obj = new JSONObject();      //extends HashMap
+	        for (int i = 1; i <= numColumns; ++i)           //iterate columns
+	        {
+	            String column_name = metadata.getColumnName(i);
+	            obj.put(column_name, results.getObject(column_name));
+	        }
+	        
+	        writer.write("{ \"index\" : { \"_index\": \"plans\", \"_type\" : \"listings\", \"_id\" : \""+count+"\" } }\n");
+	        writer.write(obj.toString());
+	        writer.write("\n");
+	        count++;
+	    }
+	//	    // Clean up
+		    conn.close();
+	}
+}
