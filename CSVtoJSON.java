59f80c600203b37a810b4d1fbef0ac5167c930fc
Vidur Chanana <vidurchanana@hotmail.com>
2017-09-05 09:16:42 -0700

    Update CSVtoJSON.java

--- CSVtoJSON.java
+++ CSVtoJSON.java
@@ -1,5 +1,3 @@
-package com.personalcapital.CodingExercise;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.sql.Connection;
@@ -15,45 +13,45 @@ public class CSVtoJSON {
 	
 	public static void main(String[] args) throws Exception{
 		
-		
-		String FILENAME1 = "C:\\Users\\vidur\\Desktop\\csvdata2500.json";
+		//Provide the location of the destination file
+		String FILENAME1 = ""; 							
 		File file1 = new File(FILENAME1);
-		
+
 		FileWriter writer = new FileWriter(file1, true);
-		
+
 		Class.forName("org.relique.jdbc.csv.CsvDriver");
 		
-	    // Create a connection. The first command line parameter is
-	    // the directory containing the .csv files.
-	    // A single connection is thread-safe for use by several threads.
-	    Connection conn = DriverManager.getConnection("jdbc:relique:csv:" + "C:\\Users\\vidur\\Desktop");	
-	    // Create a Statement object to execute the query with.
-	    // A Statement is not thread-safe.
-	    Statement stmt = conn.createStatement();
-	    String query= "SELECT * FROM csvdata LIMIT 2500 OFFSET 1";
-	    // Select the ID and NAME columns from sample.csv
-	    ResultSet results = stmt.executeQuery(query);
-	    
-	    //JSONArray json = new JSONArray();
-	    ResultSetMetaData metadata = results.getMetaData();
-	    int numColumns = metadata.getColumnCount();
-	    int count=1;
-	    while(results.next())             //iterate rows
-	    {
-	    	
-	        JSONObject obj = new JSONObject();      //extends HashMap
-	        for (int i = 1; i <= numColumns; ++i)           //iterate columns
-	        {
-	            String column_name = metadata.getColumnName(i);
-	            obj.put(column_name, results.getObject(column_name));
-	        }
-	        
-	        writer.write("{ \"index\" : { \"_index\": \"plans\", \"_type\" : \"listings\", \"_id\" : \""+count+"\" } }\n");
-	        writer.write(obj.toString());
-	        writer.write("\n");
-	        count++;
-	    }
-	//	    // Clean up
-		    conn.close();
+		// Create a connection. The first command line parameter is
+		// the directory containing the .csv files.
+		// A single connection is thread-safe for use by several threads.
+		Connection conn = DriverManager.getConnection("jdbc:relique:csv:" + " ");	//Source CSV file
+		// Create a Statement object to execute the query with.
+		// A Statement is not thread-safe.
+		Statement stmt = conn.createStatement();
+		String query= "SELECT * FROM csvdata";					//csvdata is the source file
+		// Select the ID and NAME columns from sample.csv
+		ResultSet results = stmt.executeQuery(query);
+
+		//JSONArray json = new JSONArray();
+		ResultSetMetaData metadata = results.getMetaData();
+		int numColumns = metadata.getColumnCount();
+		int count=1;
+		while(results.next())             //iterate rows
+		{
+
+		JSONObject obj = new JSONObject();      //extends HashMap
+		for (int i = 1; i <= numColumns; ++i)           //iterate columns
+		{
+		    String column_name = metadata.getColumnName(i);
+		    obj.put(column_name, results.getObject(column_name));
+		}
+
+		writer.write("{ \"index\" : { \"_index\": \"plans\", \"_type\" : \"listings\", \"_id\" : \""+count+"\" } }\n");
+		writer.write(obj.toString());
+		writer.write("\n");
+		count++;
+		}
+		// Clean up
+		conn.close();
 	}
 }
